21e1f2726f6f94c0a10170eff8cdbbcdb724ceb4
Het Prajapati <hetprajapati80@gmail.com>
2023-08-25 21:21:25 +0530

    intial Commit

--- inheritance.dart
+++ inheritance.dart
@@ -1,3 +1,25 @@
 void main() {
+  //Object oriented programming (OOPs)
+  // Is-A relationship
   print("INheritance");
+  Car car = Car();
+  print(car.isEngineWorking);
+  print(car.noOfWheels);
+}
+
+class Vehicle {
+  int speed = 10;
+  bool isEngineWorking = false;
+  bool isLightOn = true;
+
+  void accelerate() {
+    speed += 10;
+  }
+}
+
+class Car extends Vehicle {
+  int noOfWheels = 4;
+  void printSomething() {
+    print(noOfWheels);
+  }
 }
