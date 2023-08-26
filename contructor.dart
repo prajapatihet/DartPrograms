94aa60bb9170bd6330a2ad852af657882c76436f
Hetkumar Prajapati <hetgoraj@gmail.com>
2023-08-26 19:34:20 +0530

    intial Commit

--- contructor.dart
+++ contructor.dart
@@ -1,1 +1,22 @@
+void main() {
+  Cookie cookie = Cookie("Circle", 15.2);
+  print(cookie.shape);
+  print(cookie.size);
+}
 
+class Cookie {
+  String? shape;
+  double? size;
+  Cookie(this.shape, this.size) {
+    print("Constructor Called...");
+    baking();
+  }
+
+  void baking() {
+    print("Baking Cookies...");
+  }
+
+  bool isCooling() {
+    return false;
+  }
+}
