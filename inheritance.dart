474b6bc93bf0441c16af0d7c5db962641b20591f
Hetkumar Prajapati <hetgoraj@gmail.com>
2023-08-26 20:25:28 +0530

    initial Commit

--- inheritance.dart
+++ inheritance.dart
@@ -3,15 +3,28 @@ void main() {
   // Is-A relationship
   print("INheritance");
   Car car = Car();
+  print("car from Car");
   print(car.isEngineWorking);
   print(car.noOfWheels);
+  print("truck from Truck");
+  Truck truck = Truck();
+  print(truck.noOfWheels);
+  print("car from Vehicle");
+  Vehicle car1 = Car();
+  print((car1 as Car).noOfWheels);
 }
 
-class Vehicle {
+// Multiple inheriance is not allowed in Dart.
+class SomeValue {
   int speed = 10;
+  void accelerate() {
+    speed += 20;
+  }
+}
+
+class Vehicle extends SomeValue {
   bool isEngineWorking = false;
   bool isLightOn = true;
-
   void accelerate() {
     speed += 10;
   }
@@ -19,6 +32,13 @@ class Vehicle {
 
 class Car extends Vehicle {
   int noOfWheels = 4;
+  void printSomething() {
+    print(noOfWheels);
+  }
+}
+
+class Truck extends Vehicle {
+  int noOfWheels = 8;
   void printSomething() {
     print(noOfWheels);
   }
