a50b234f4b30cef16a4bf62d3b606707b4429ee6
Het Prajapati <hetprajapati80@gmail.com>
2023-08-28 18:54:56 +0530

    Initial Commit

--- polymorphism.dart
+++ polymorphism.dart
@@ -1,3 +1,31 @@
 void main() {
   print("Polymorphism");
+  // print current date and time as per system
+  final date = DateTime.now();
+  print(date);
+
+  Animal cat = Cat();
+  cat.sound();
+  cat = Dog();
+  cat.sound();
+}
+
+class Animal {
+  void sound() {
+    print("animal making sound.....");
+  }
+}
+
+class Cat extends Animal {
+  @override
+  void sound() {
+    print("Cat making sound.......");
+  }
+}
+
+class Dog extends Animal {
+  @override
+  void sound() {
+    print("Dog making sound.....");
+  }
 }
