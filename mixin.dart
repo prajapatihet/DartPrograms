6b74234c334b0b653773010559a362f8cc9881da
Hetkumar Prajapati <hetgoraj@gmail.com>
2023-08-28 19:45:40 +0530

    Initial Commit

--- mixin.dart
+++ mixin.dart
@@ -1,3 +1,33 @@
 void main() {
   print("Mixin");
+  final anim = Animal();
+  anim.fun();
+
+  Object anim1 = 'Hello';
+  print(anim1);
+}
+
+mixin Jump {
+  int jumping = 10;
+}
+mixin Scream {
+  bool isScreaming = false;
+}
+
+// It may have multiple mixin with single class
+class Animal with Jump, Scream {
+  void fun() {
+    print(jumping);
+    print(isScreaming);
+    // mixin can use with keyboard
+    // class can use extends keyboard
+  }
+}
+
+class Cat extends Animal {
+  void func() {
+    print(jumping);
+    // if we are not using 'jumping' variable in  Animal class
+    // we cann't use it here
+  }
 }
