d07b8a5463c877e24e13ad53775facf8859db888
Hetkumar Prajapati <hetgoraj@gmail.com>
2023-08-29 19:20:13 +0530

    intial commit

--- moreList.dart
+++ moreList.dart
@@ -1,16 +1,25 @@
 void main() {
-  List students = [
-    Student("Baroda"),
-    Student("Ahms"),
-    Student("Anand"),
-    "Hello",
-    20,
-    false
-  ];
-  print(students[0].name);
+  final ahms = Student('Ahms');
+  List students = [Student("Baroda"), ahms, Student("Anand")];
+  print(students);
+
+  // for adding element at last position
+  students.add(Student("name"));
+  print(students);
+
+  // for adding at first or any position
+  students.insert(0, Student("Het"));
+  print(students);
+
+  //for removing element
+  students.remove(ahms);
+  print(students);
 }
 
 class Student {
   final String name;
   Student(this.name);
+
+  @override
+  String toString() => 'Student: $name';
 }
